a00d673f1e65bead709a99fe3b39371f9acc4218
KuraiNT <cho71167@gmail.com>
2023-03-03 16:58:57 +0700

    cài đặt template engine

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+const app = express();
+const port = 3001;
+
+// HTTP logger
+app.use(morgan("combined"));
+
+// Template engine
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/view", (req, res) => {
+  res.render("view");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
